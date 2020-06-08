ea677c33078e7e5754324ae2692c3dd5f1aee936
GitHub Build Agent <sdm.mit@t-online.de>
2020-06-08 10:34:48 +0000

    Update nightlies from github actions

--- nightly/master/prod/MR401/src/src-thirdparty/script/@formatjs/intl-pluralrules/dist/locale-data/de.js
+++ nightly/master/prod/MR401/src/src-thirdparty/script/@formatjs/intl-pluralrules/dist/locale-data/de.js
@@ -5,5 +5,5 @@ if (Intl.PluralRules && typeof Intl.PluralRules.__addLocaleData === 'function') 
   var s = String(n).split('.'), v0 = !s[1];
   if (ord) return 'other';
   return n == 1 && v0 ? 'one' : 'other';
-}}},"aliases":{},"parentLocales":{},"availableLocales":["de"]})
+}}},"availableLocales":["de"]})
 }
